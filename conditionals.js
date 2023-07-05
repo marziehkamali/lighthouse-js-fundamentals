146066addc3581f2c1f6fd5c5c0ea726b653b290
marzieh kamali <mayhey1@gmail.com>
2023-07-05 10:32:44 +0000

    Use conditionals to tell people about the weather

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,123 @@
+const temperature = 45;
+
+if (temperature < 0) {
+  console.log("make sure you pick out a scarf!");
+} else if (temperature < 15 ) {
+  console.log("Short sleeves wont cut it!");
+} else {
+  console.log("short sleeves are fine!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
+
+
+/* const raining = false;
+const cold = true;
+
+if (raining) {
+console.log("Dont forget to bring an umbrella!");
+}
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} 
+console.log("now you are ready to go outside!");
+*/
+
+
+/*
+If...else statements allow you to execute 
+certain pieces of code based on a condition, or set of conditions, being met.
+*/
+
+/*let weather = raining
+
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+*/
+
+
+/*
+EXAMPLE:
+var price = 20.00
+var money = 15.00
+if (money >= price) {
+  console.log("buy the hammer");
+} else { 
+  console.log("dont buy the hammer");
+}
+*/
+
+/* change the value of `number` to test your if...else statement
+var number = 21;
+
+// Note that a strict equality operator === checks the equality of operands without type conversion.
+if (0 === number % 2) {
+    console.log("even");
+} else {
+    console.log("odd");
+    */
+
+
+
+
+
+/* 
+var musicians = 0
+
+if (musicians <= 0) { 
+  console.log("not a group");
+} else if (musicians == 1) {
+  console.log("solo");
+} else if (musicians == 2) {
+  console.log("duet");
+} else if (musicians == 3) {
+  console.log("trio");
+} else if (musicians == 4) {
+  console.log("quartet");
+} else {
+  console.log("this is a large group");
+}
+*/
+
+
+/*
+if (/ *this expression is true* /) {
+  // run this code
+} else {
+  // run this code
+}
+*/
+
+/*var weather = "sunny";  // declaration of variable
+
+if (weather === "snowy") {  // conditional statement
+  console.log("wear a coat");
+} else if ( weather === "rainy") {  //another conditional statement
+  console.log("wear a rainjacket");
+} else { 
+  console.log("wear a dress")
+}
+*/
+
+
+/*
+var runner = "Kendyll";
+var position = 2;
+var medal;
+
+if (position ===1) {
+  medal = "gold";
+} else if (position === 2) {
+  medal = "silver";
+} else if (position === 3) {
+  medal = "bronze";;
+} else {
+  medal = "pat on the back";
+}
+
+console.log(runner + " received a " + medal + " medal.");
+*/
+
